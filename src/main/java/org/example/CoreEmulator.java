f1ef54c0b9fa294877a8b10e8306c7aad632809d
andrea <rosanovaandrea82@gmail.com>
2026-08-21 18:07:00 +0200

    inizializzzione parte di test

--- src/main/java/org/example/CoreEmulator.java
+++ src/main/java/org/example/CoreEmulator.java
@@ -7,7 +7,7 @@ import java.nio.channels.FileChannel;
 
 public class CoreEmulator {
 
-    private static final int[] RAM = new int[65536];
+    private static  int[] RAM = new int[65536];
 
     //8 bit registers emulators
     private int a,b,c,d,e,h,l;
@@ -21,8 +21,8 @@ public class CoreEmulator {
 
     private int temp, mask;
 
-    public static void initializeRAM(String[] files) throws IOException {
-        int pointer = 0;
+    public static int initializeRAM(String[] files, int startWrite) throws IOException {
+        int pointer = startWrite;
         ByteBuffer byteBuffer = ByteBuffer.allocate(2048);
 
         for (String file : files) {
@@ -40,19 +40,25 @@ public class CoreEmulator {
                 }
             }
         }
+
+        return pointer;
     }
 
-    public CoreEmulator(){
-        a = b = c = e = h = l = sp = pc = bc = hl = temp = mask = 0;
-        z = s = p = cy = ac = false;
+    // solo a scopo di test
+    public static void setRAM(int[] newRAM){
+        RAM = newRAM;
     }
+
+
 
     public void emulationProcess(){
 
         int opcode = RAM[pc];
 
 
-        switch (opcode){
+        switch (opcode) {
+
+
             case 0X00:
                 pc++;
                 break;
@@ -64,11 +70,11 @@ public class CoreEmulator {
             case 0x02:
                 temp = (b << 8 | c);
                 RAM[temp] = a;
-                pc ++;
+                pc++;
                 break;
             case 0x03:
                 bc = (b << 8 | c);
-                bc ++;
+                bc++;
                 bc = bc & 0xffff;
                 c = bc & 0xff;
                 b = bc >>> 8;
@@ -76,7 +82,7 @@ public class CoreEmulator {
                 break;
             case 0X04:
                 ac = (b & 0x0f) == 0x0f;
-                b = (b + 1)& 0xff;
+                b = (b + 1) & 0xff;
                 s = (b >>> 7) != 0;
                 z = b == 0;
                 p = (Integer.bitCount(b) % 2) == 0;
@@ -85,18 +91,18 @@ public class CoreEmulator {
             case 0x05:
                 ac = (b & 0x0f) == 0x00;
                 b = (b - 1) & 0xff;
-                s = (b >>> 7)  != 0;
+                s = (b >>> 7) != 0;
                 z = b == 0;
                 p = (Integer.bitCount(b) % 2) == 0;
                 pc++;
                 break;
             case 0x06:
-                b = RAM[pc+1];
-                pc +=2;
+                b = RAM[pc + 1];
+                pc += 2;
                 break;
             case 0x07:
                 cy = (a >>> 7) == 1;
-                a =  a >>> 7 | a << 1;
+                a = a >>> 7 | a << 1;
                 a = a & 0xFF;
                 pc++;
                 break;
@@ -104,9 +110,9 @@ public class CoreEmulator {
                 pc++;
                 break;
             case 0x09:
-                 hl = h << 8 | l;
-                 bc = b << 8 | c;
-                 hl = hl + bc;
+                hl = h << 8 | l;
+                bc = b << 8 | c;
+                hl = hl + bc;
                 cy = hl > 0xFFFF;
                 hl = hl & 0xFFFF;
                 h = hl >>> 8;
@@ -114,10 +120,10 @@ public class CoreEmulator {
                 pc++;
                 break;
             case 0x0a:
-                 bc = b << 8 | c;
-                 a = RAM[bc];
-                 pc++;
-                 break;
+                bc = b << 8 | c;
+                a = RAM[bc];
+                pc++;
+                break;
             case 0x0b:
                 bc = b << 8 | c;
                 bc--;
@@ -136,15 +142,15 @@ public class CoreEmulator {
                 break;
             case 0x0d:
                 ac = (c & 0x0f) == 0x00;
-                c = (c-1) & 0xff;
+                c = (c - 1) & 0xff;
                 s = (c >>> 7) != 0;
                 z = c == 0;
                 p = Integer.bitCount(c) % 2 == 0;
                 pc++;
                 break;
             case 0x0e:
-                c = RAM[pc+1];
-                pc +=2;
+                c = RAM[pc + 1];
+                pc += 2;
                 break;
             case 0x0f:
                 cy = (a & 0x01) == 1;
@@ -155,8 +161,8 @@ public class CoreEmulator {
                 pc++;
                 break;
             case 0x11:
-                e = RAM[pc+1];
-                d = RAM[pc+2];
+                e = RAM[pc + 1];
+                d = RAM[pc + 2];
                 pc += 3;
                 break;
             case 0x12:
@@ -189,13 +195,13 @@ public class CoreEmulator {
                 pc++;
                 break;
             case 0x16:
-                d = RAM[pc+1];
+                d = RAM[pc + 1];
                 pc += 2;
                 break;
             case 0x17:
-                temp =  cy ? 0x01: 0x00;
+                temp = cy ? 0x01 : 0x00;
                 cy = (a >>> 7) == 1;
-                a =  (a << 1 | temp) & 0xff;
+                a = (a << 1 | temp) & 0xff;
                 pc++;
                 break;
             case 0x18:
@@ -218,7 +224,7 @@ public class CoreEmulator {
                 break;
             case 0x1b:
                 de = (d << 8) | e;
-                de --;
+                de--;
                 d = (de >>> 8) & 0xFF;
                 e = de & 0xFF;
                 pc++;
@@ -240,8 +246,8 @@ public class CoreEmulator {
                 pc++;
                 break;
             case 0x1e:
-                e = RAM[pc+1];
-                pc +=2;
+                e = RAM[pc + 1];
+                pc += 2;
                 break;
             case 0x1f:
                 cy = (a & 0x01) == 0x01;
@@ -254,14 +260,14 @@ public class CoreEmulator {
                 pc++;
                 break;
             case 0x21:
-                h = RAM[pc+2];
-                l  = RAM[pc+1];
+                h = RAM[pc + 2];
+                l = RAM[pc + 1];
                 pc += 3;
                 break;
             case 0x22:
-                temp = (RAM[pc+2] & 0xff) << 8 | RAM[pc+1] & 0xff;
+                temp = (RAM[pc + 2] & 0xff) << 8 | RAM[pc + 1] & 0xff;
                 RAM[temp] = l;
-                RAM[temp +1] = h;
+                RAM[temp + 1] = h;
                 pc += 3;
                 break;
             case 0x23:
@@ -286,43 +292,43 @@ public class CoreEmulator {
                 pc++;
                 break;
             case 0x31:
-                sp = (RAM[pc+2] & 0xff) << 8 | RAM[pc+1] & 0xff;
+                sp = (RAM[pc + 2] & 0xff) << 8 | RAM[pc + 1] & 0xff;
                 pc += 3;
                 break;
             case 0x32:
-                temp = (RAM[pc + 2] & 0xff) << 8 | RAM[pc+1] & 0xff;
+                temp = (RAM[pc + 2] & 0xff) << 8 | RAM[pc + 1] & 0xff;
                 RAM[temp] = a & 0xff;
                 pc += 3;
                 break;
             case 0x36:
                 hl = (h & 0xff) << 8 | l & 0xff;
-                temp = RAM[pc+1] & 0xff;
+                temp = RAM[pc + 1] & 0xff;
                 RAM[hl] = temp;
                 pc += 2;
                 break;
             case 0x3a:
                 temp = (RAM[pc + 2] & 0xff) << 8 | RAM[pc + 1] & 0xff;
                 a = RAM[temp] & 0xff;
-                pc+=3;
+                pc += 3;
                 break;
             case 0x3e:
-                a = RAM[pc+1] & 0xff;
+                a = RAM[pc + 1] & 0xff;
                 pc += 2;
                 break;
             case 0x56:
                 hl = (h & 0xff) << 8 | l & 0xff;
                 d = RAM[hl] & 0xff;
-                pc ++;
+                pc++;
                 break;
             case 0x5e:
                 hl = (h & 0xff) << 8 | l & 0xff;
                 e = RAM[hl] & 0xff;
-                pc ++;
+                pc++;
                 break;
             case 0x66:
                 hl = (h & 0xff) << 8 | l & 0xff;
                 h = RAM[hl] & 0xff;
-                pc ++;
+                pc++;
                 break;
             case 0x6f:
                 l = a;
@@ -331,7 +337,7 @@ public class CoreEmulator {
             case 0x77:
                 hl = (h & 0xff) << 8 | l & 0xff;
                 RAM[hl] = c & 0xff;
-                pc ++;
+                pc++;
                 break;
             case 0x7a:
                 a = d;
@@ -348,7 +354,7 @@ public class CoreEmulator {
             case 0x7e:
                 hl = (h & 0xff) << 8 | l & 0xff;
                 a = RAM[hl] & 0xff;
-                pc ++;
+                pc++;
                 break;
             case 0xa7:
                 a = a & a;
@@ -375,17 +381,17 @@ public class CoreEmulator {
                 pc++;
                 break;
             case 0xc2:
-                temp = (RAM[pc + 2] & 0xff) << 8 | RAM[pc+1] & 0xff;
+                temp = (RAM[pc + 2] & 0xff) << 8 | RAM[pc + 1] & 0xff;
                 mask = z ? 0 : -1;
                 pc = temp & mask | (pc + 3) & ~mask;
                 break;
             case 0xc3:
-                temp = (RAM[pc + 2] & 0xff) << 8 | RAM[pc+1] & 0xff;
+                temp = (RAM[pc + 2] & 0xff) << 8 | RAM[pc + 1] & 0xff;
                 pc = temp;
                 break;
             case 0xc5:
-                RAM[sp-1] = b;
-                RAM[sp-2] = c;
+                RAM[sp - 1] = b;
+                RAM[sp - 2] = c;
                 sp -= 2;
                 pc++;
                 break;
@@ -402,14 +408,14 @@ public class CoreEmulator {
                 break;
             case 0xc9:
                 pc = RAM[sp] & 0xff;
-                pc = pc | ( (RAM[sp + 1] & 0xff) << 8);
+                pc = pc | ((RAM[sp + 1] & 0xff) << 8);
                 sp += 2;
                 break;
             case 0xcd:
-                RAM[sp-2] =  (pc + 3) >>> 8;
-                RAM[sp-1] =  (pc + 3) & 0xff;
+                RAM[sp - 2] = (pc + 3) >>> 8;
+                RAM[sp - 1] = (pc + 3) & 0xff;
                 sp -= 2;
-                pc = (RAM[pc + 2] & 0xff) << 8 | RAM[pc+1] & 0xff;
+                pc = (RAM[pc + 2] & 0xff) << 8 | RAM[pc + 1] & 0xff;
                 break;
             case 0xd1:
                 e = RAM[sp] & 0xff;
@@ -421,8 +427,8 @@ public class CoreEmulator {
                 pc += 2;
                 break;
             case 0xd5:
-                RAM[sp-2] = e;
-                RAM[sp-1] = d;
+                RAM[sp - 2] = e;
+                RAM[sp - 1] = d;
                 sp -= 2;
                 pc++;
                 break;
@@ -430,11 +436,11 @@ public class CoreEmulator {
                 l = RAM[sp] & 0xff;
                 h = RAM[sp + 1] & 0xff;
                 sp += 2;
-                pc ++;
+                pc++;
                 break;
             case 0xe5:
-                RAM[sp-2] = l;
-                RAM[sp-1] = h;
+                RAM[sp - 2] = l;
+                RAM[sp - 1] = h;
                 sp -= 2;
                 pc++;
                 break;
@@ -462,7 +468,7 @@ public class CoreEmulator {
                 p = (RAM[sp] >>> 2 & 0x01) == 1;
                 ac = (RAM[sp] >>> 4 & 0x01) == 1;
                 z = (RAM[sp] >>> 6 & 0x01) == 1;
-                s =(RAM[sp] >>> 7 & 0x01) == 1;
+                s = (RAM[sp] >>> 7 & 0x01) == 1;
                 a = RAM[sp + 1];
                 sp += 2;
                 pc++;
@@ -470,7 +476,7 @@ public class CoreEmulator {
             case 0xf5:
                 temp = 0;
                 temp = temp | (cy ? 0x01 : 0x00);
-                temp = temp |  0x01 << 1;
+                temp = temp | 0x01 << 1;
                 temp = temp | (p ? 0x01 : 0x00) << 2;
                 temp = temp | (ac ? 0x01 : 0x00) << 4;
                 temp = temp | (z ? 0x01 : 0x00) << 6;
@@ -494,9 +500,172 @@ public class CoreEmulator {
                 s = temp >>> 7 != 0;
                 pc += 2;
                 break;
+            default:
+                throw new RuntimeException("opcode non gestito e riconosciuto");
 
-
+            }
         }
 
+    public CoreEmulator(){
+        a = b = c = e = h = l = sp = pc = bc = hl = temp = mask = 0;
+        z = s = p = cy = ac = false;
     }
+
+    //metodi da utilizzare solo nei test :
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
+
+    public int getE() {
+        return e;
+    }
+
+    public void setE(int e) {
+        this.e = e;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+
+    public int getL() {
+        return l;
+    }
+
+    public void setL(int l) {
+        this.l = l;
+    }
+
+    public int getBc() {
+        return bc;
+    }
+
+    public void setBc(int bc) {
+        this.bc = bc;
+    }
+
+    public int getHl() {
+        return hl;
+    }
+
+    public void setHl(int hl) {
+        this.hl = hl;
+    }
+
+    public int getDe() {
+        return de;
+    }
+
+    public void setDe(int de) {
+        this.de = de;
+    }
+
+    public int getSp() {
+        return sp;
+    }
+
+    public void setSp(int sp) {
+        this.sp = sp;
+    }
+
+    public int getPc() {
+        return pc;
+    }
+
+    public void setPc(int pc) {
+        this.pc = pc;
+    }
+
+    public boolean isZ() {
+        return z;
+    }
+
+    public void setZ(boolean z) {
+        this.z = z;
+    }
+
+    public boolean isS() {
+        return s;
+    }
+
+    public void setS(boolean s) {
+        this.s = s;
+    }
+
+    public boolean isP() {
+        return p;
+    }
+
+    public void setP(boolean p) {
+        this.p = p;
+    }
+
+    public boolean isCy() {
+        return cy;
+    }
+
+    public void setCy(boolean cy) {
+        this.cy = cy;
+    }
+
+    public boolean isAc() {
+        return ac;
+    }
+
+    public void setAc(boolean ac) {
+        this.ac = ac;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }
+
+    public int getMask() {
+        return mask;
+    }
+
+    public void setMask(int mask) {
+        this.mask = mask;
+    }
+
+
+
+
 }
