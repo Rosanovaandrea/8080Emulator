a94110c3bfcc331c206cfd70d3003526a78848f3
andrea <rosanovaandrea82@gmail.com>
2026-09-12 17:42:42 +0200

    aggiunta instruzione per implementare lo shiftwer hardware

--- src/main/java/org/example/Machine.java
+++ src/main/java/org/example/Machine.java
@@ -2,6 +2,58 @@ package org.example;
 
 public class Machine {
       private CoreEmulator emulator;
+      private int inutBus;
+      private int outputBus;
+      private final int inOpcode = 0xdb;
+      private final int outOpcode = 0xd3;
+      private int shiftRegister = 0;
+      private byte offset = 0;
+
+      public void execution (){
+
+            while (true){
+                  int opcode = emulator.getOpcode();
+
+                  switch (opcode) {
+                        case inOpcode:
+                              //inoperations
+                              emulator.nextOpceAfetHw();
+                              break;
+                        case outOpcode:
+                              //outputoperations
+                              emulator.nextOpceAfetHw();
+                              break;
+                        default:
+                              emulator.emulationProcess();
+                        }
+
+            }
+
+
+            }
+
+
+       public void intputHandler(){
+
+       }
+
+
+
+
+
+      public void setShiftRegister(int data){
+            data = data & 0xff;
+            shiftRegister = shiftRegister >>> 8 | data << 8;
+      }
+
+      public void setOffset(int data){
+            offset = (byte) (data & 0x07);
+      }
+
+      public int getShiftRegisterWithOffset(){
+            return (shiftRegister >>> (8 - offset)) & 0xff;
+      }
+
 
 
 }
